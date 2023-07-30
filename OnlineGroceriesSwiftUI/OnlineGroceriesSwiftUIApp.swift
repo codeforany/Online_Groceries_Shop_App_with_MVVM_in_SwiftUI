26d4902178993bd390e7b39968351829fe66e6b2
codeforany <130132429+codeforany@users.noreply.github.com>
2023-07-30 22:05:22 +0530

    Initial Commit

---
+++ OnlineGroceriesSwiftUI/OnlineGroceriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesSwiftUIApp.swift
+//  OnlineGroceriesSwiftUI
+//
+//  Created by CodeForAny on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
