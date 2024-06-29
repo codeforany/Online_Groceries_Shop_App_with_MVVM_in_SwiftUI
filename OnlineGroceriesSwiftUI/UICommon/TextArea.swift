369c24885403793a7e7c06c14d578d48ef2694d8
CodeForAny <130132429+codeforany@users.noreply.github.com>
2024-06-29 18:10:10 +0530

    Write A Review

---
+++ OnlineGroceriesSwiftUI/UICommon/TextArea.swift
@@ -0,0 +1,18 @@
+//
+//  TextArea.swift
+//  OnlineGroceriesSwiftUI
+//
+//  Created by CodeForAny on 28/06/24.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextArea()
+}
---
+++ OnlineGroceriesSwiftUI/View/Account/WriteReviewView.swift
@@ -0,0 +1,18 @@
+//
+//  WriteReviewView.swift
+//  OnlineGroceriesSwiftUI
+//
+//  Created by CodeForAny on 28/06/24.
+//
+
+import SwiftUI
+
+struct WriteReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WriteReviewView()
+}
